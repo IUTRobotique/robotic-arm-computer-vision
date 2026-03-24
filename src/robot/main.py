33eb2539074f36eb16bee16a067d91c31213d1f0
mflorent <sandi.florent9@gmail.com>
2026-03-24 10:45:12 +0100

    Moving the robot accordingly to the simulation

--- src/robot/main.py
+++ src/robot/main.py
@@ -6,11 +6,13 @@ import argparse
 import os
 import time
 from pathlib import Path
+import sim_to_real
 
 import numpy as np
 from stable_baselines3 import PPO, SAC, TD3
 
 from robot_env.push_in_hole_env import PushInHoleEnv as PushEnv
+
 
 ALGO_CLS = {
     "sac": SAC,
@@ -88,6 +90,7 @@ if __name__ == "__main__":
 
     rewards, successes, distances = [], [], []
 
+    sim_to_real.init_real_robot()
     for ep in range(args.episodes):
         obs, _ = env.reset()
         done = False
@@ -95,6 +98,10 @@ if __name__ == "__main__":
 
         while not done:
             action, _ = model.predict(obs, deterministic=True)
+            obs, reward, terminated, truncated, info = env.step(action)
+            motor_joints = env._inner.sim.get_qpos()
+            sim_to_real.update_real_robot_position(motor_joints)
+
             obs, reward, terminated, truncated, info = env.step(action)
             env.render()
             total_reward += reward
@@ -110,6 +117,7 @@ if __name__ == "__main__":
         print(f"Ep {ep+1:3d}: reward={total_reward:7.2f}  "
               f"success={info['is_success']}  dist={dist_value:.4f}")
 
+    sim_to_real.close_real_robot()
     env.close()
 
     print(f"\n--- {args.algo} | {args.episodes} episodes ---")
